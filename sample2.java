a68ce2d1b722f3a123e606f7238f8887ee5bd884
Anish Khemani <91882411+daxamengineer@users.noreply.github.com>
2024-09-27 18:49:17 +0530

    Update sample2.java

--- sample2.java
+++ sample2.java
@@ -2,8 +2,8 @@ public class sample2 {
   int x = 5;
 
   public static void main(String[] args) {
-    MyClass myObj1 = new MyClass();  // Object 1
-    MyClass myObj2 = new MyClass();  // Object 2
+    sample2 myObj1 = new sample2();  // Object 1
+    sample2 myObj2 = new sample2();  // Object 2
     System.out.println(myObj1.x);
     System.out.println(myObj2.x);
   }
